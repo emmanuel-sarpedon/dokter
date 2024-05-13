ffc3d46d925e34ff8a2076d349a1f14560b9a332
Emmanuel SARPEDON <emmanuel.sarpedon@gmail.com>
2024-05-13 14:48:10 +0400

    feat: update favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,8 @@ import { cn } from "@/lib/utils";
 const DosisFont = Dosis({ subsets: ["latin"], variable: "--font-dosis" });
 
 export const metadata = {
+  icons:
+    "data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>🇷🇪</text></svg>",
   title:
     "Dokter.re - Trouvez facilement un médecin généraliste ou un spécialiste à proximité",
   description:
